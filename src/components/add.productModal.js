510e5fd2acc68bdba6103377e41e91bbaef6d1a3
Morning <morning@mstars-lab3-05s-iMac.local>
2023-02-07 12:43:50 +0800

    day4

--- src/components/add.productModal.js
+++ src/components/add.productModal.js
@@ -49,20 +49,51 @@ const ProductModal = (data) => {
           <textarea cols="4" rows="5"></textarea>
         </div>
       </div>
-      <div className="row col-6">
-        <div className="d-flex flex-column">
-          <label>Product Images</label>
-          <input type="file" />
+      <div className="row col-6 modalRight">
+        <div className="d-flex flex-column gap-3">
+          <div className="d-flex flex-column">
+            <label>Product Images</label>
+            <input type="file" className="fileInput" />
+          </div>
+          <div className="d-flex flex-column">
+            <label>Add size</label>
+            <div className="d-flex gap-3">
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="XXS" />
+                <label for="XXS">XXS</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="XS" />
+                <label for="XS">XS</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="S" />
+                <label for="S">S</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="M" />
+                <label for="M">M</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="L" />
+                <label for="L">L</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="XL" />
+                <label for="XL">XL</label>
+              </div>
+              <div className="d-flex align-items-center gap-2">
+                <input type="checkbox" id="XXL" />
+                <label for="XXL">XXL</label>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex flex-column">
+            <label>Product Date</label>
+            <input type="date" />
+          </div>
+          <button className="btn btn-primary">Add product</button>
         </div>
-        <div className="d-flex flex-column">
-          <label>Add size /sm/</label>
-          <input type="number" />
-        </div>
-        <div className="d-flex flex-column">
-          <label>Product Date</label>
-          <input type="date" />
-        </div>
-        <button className="btn btn-primary">Add product</button>
       </div>
     </div>
   );
