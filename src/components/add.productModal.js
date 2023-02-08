bf104cfc9a29d78f3907c2c72adde1da7ed69096
J@ck Sp@rrow <jack.jantsaa@gmail.com>
2023-02-08 23:34:00 +0800

    day5

--- src/components/add.productModal.js
+++ src/components/add.productModal.js
@@ -6,7 +6,7 @@ import { CloudinaryImage } from "@cloudinary/url-gen";
 import { AdvancedImage } from "@cloudinary/react";
 import axios, { Axios } from "axios";
 
-const ProductModal = (data) => {
+const ProductModal = ({ show, setShow }) => {
   const init = {
     productName: "",
     categoryID: "",
@@ -19,10 +19,13 @@ const ProductModal = (data) => {
     createDate: Date.now(),
     createUser: "",
   };
+  const onClose = () => {
+    setShow(false);
+  };
 
   const [category, setCategory] = useState([]);
   const [product, setProduct] = useState(init);
-  const [image, setImage] = useState("");
+
   useEffect(() => {
     fetch("http://localhost:9000/api/productCat")
       .then((res) => res.json())
@@ -40,7 +43,7 @@ const ProductModal = (data) => {
     });
     setProduct(init);
   }
-  async function uploadImage(files) {
+  const uploadImage = async (files) => {
     console.log(files);
     if (files.length == 1) {
       const formData = new FormData();
@@ -54,10 +57,10 @@ const ProductModal = (data) => {
         });
     } else {
       const slideImages = [];
-      // const slideImg = [];
       for (let i = 0; i < files.length; i++) {
         slideImages.push(files[i]);
       }
+      console.log(slideImages);
       const promose = await Promise.all(
         slideImages.map((file) => {
           const formData = new FormData();
@@ -68,106 +71,138 @@ const ProductModal = (data) => {
             formData
           );
         })
-      );
-      // setProduct({ ...product, images: slideImg });
+      ).then((res) => {
+        const slideImg = [];
+        res.map((img) => {
+          console.log(img);
+          slideImg.push(img.data.secure_url);
+        });
+        console.log(slideImg);
+        setProduct({ ...product, images: slideImg });
+      });
     }
-  }
+  };
   const myImage = new CloudinaryImage("sample", {
     cloudName: "duo1znrio",
   }).resize(fill().width(10).height(10));
 
   return (
-    <div className="d-flex row text-secondary">
-      <div className="row col-6 gap-3">
-        <div className="d-flex flex-column">
-          <label>Product Name</label>
-          <input
-            type={"text"}
-            placeholder="Enter product name !!!"
-            value={product.productName}
-            onChange={(e) => {
-              setProduct({ ...product, productName: e.target.value });
-            }}
-          />
-        </div>
-        <div className="row">
-          <div className="d-flex flex-column col-8">
-            <label>Category</label>
-            <select
-              value={product.categoryID}
-              onChange={(e) => {
-                setProduct({ ...product, categoryID: e.target.value });
-              }}
+    <div
+      className="modal"
+      style={{ display: show ? "flex" : "none" }}
+      onClick={() => setShow(false)}
+    >
+      <div
+        class="modal-dialog"
+        role="document"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div class="modal-content">
+          <div class="d-flex justify-content-around">
+            <h3 className="text-secondary">Add product</h3>
+            <button
+              className="close border-0 bg-none"
+              onClick={() => setShow(false)}
             >
-              <option>Category</option>
-              {category.map((e) => {
-                return <option value={e.category}>{e.category}</option>;
-              })}
-            </select>
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-          <div className="d-flex flex-column col-4">
-            <label>Price</label>
-            <input
-              type={"number"}
-              value={product.price}
-              onChange={(e) => {
-                setProduct({ ...product, price: e.target.value });
-              }}
-            />
+          <div className=" row text-secondary">
+            <div className="row col-6 gap-3">
+              <div className="d-flex flex-column">
+                <label>Product Name</label>
+                <input
+                  type={"text"}
+                  placeholder="Enter product name !!!"
+                  value={product.productName}
+                  onChange={(e) => {
+                    setProduct({ ...product, productName: e.target.value });
+                  }}
+                />
+              </div>
+              <div className="row">
+                <div className="d-flex flex-column col-8">
+                  <label>Category</label>
+                  <select
+                    value={product.categoryID}
+                    onChange={(e) => {
+                      setProduct({ ...product, categoryID: e.target.value });
+                    }}
+                  >
+                    <option>Category</option>
+                    {category.map((e) => {
+                      return <option value={e.category}>{e.category}</option>;
+                    })}
+                  </select>
+                </div>
+                <div className="d-flex flex-column col-4">
+                  <label>Price</label>
+                  <input
+                    type={"number"}
+                    value={product.price}
+                    onChange={(e) => {
+                      setProduct({ ...product, price: e.target.value });
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <label>Brand</label>
+                <input type="text" />
+              </div>
+              <div className="d-flex flex-column">
+                <label>Description</label>
+                <textarea
+                  value={product.desc}
+                  onChange={(e) => {
+                    setProduct({ ...product, desc: e.target.value });
+                  }}
+                  cols="4"
+                  rows="5"
+                ></textarea>
+              </div>
+            </div>
+            <div className="row col-6 modalRight">
+              <div className="d-flex flex-column gap-3">
+                <div className="d-flex flex-column">
+                  <label>Thumb image</label>
+                  <input
+                    type="file"
+                    className="fileInput"
+                    onChange={(e) => {
+                      uploadImage(e.target.files);
+                    }}
+                  />
+                </div>
+                <div className="d-flex flex-column">
+                  <label>Slide Images</label>
+                  <input
+                    multiple
+                    type="file"
+                    className="fileInput"
+                    onChange={(e) => {
+                      uploadImage(e.target.files);
+                    }}
+                  />
+                </div>
+                <div className="d-flex flex-column">
+                  <label>Created User</label>
+                  <input type="text" />
+                </div>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    onClose();
+                    onSave(product);
+                  }}
+                >
+                  Add product
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="d-flex flex-column">
-          <label>Brand</label>
-          <input type="text" />
-        </div>
-        <div className="d-flex flex-column">
-          <label>Description</label>
-          <textarea
-            value={product.desc}
-            onChange={(e) => {
-              setProduct({ ...product, desc: e.target.value });
-            }}
-            cols="4"
-            rows="5"
-          ></textarea>
-        </div>
-      </div>
-      <div className="row col-6 modalRight">
-        <div className="d-flex flex-column gap-3">
-          <div className="d-flex flex-column">
-            <label>Thumb image</label>
-            <input
-              type="file"
-              className="fileInput"
-              onChange={(e) => {
-                uploadImage(e.target.files);
-              }}
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <label>Slide Images</label>
-            <input
-              multiple
-              type="file"
-              className="fileInput"
-              onChange={(e) => {
-                uploadImage(e.target.files);
-              }}
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <label>Created User</label>
-            <input type="text" />
-          </div>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              // onClose();
-              onSave(product);
-            }}
-          >
-            Add product
-          </button>
         </div>
       </div>
     </div>
--- src/outlets/admin.products.js
+++ src/outlets/admin.products.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import ProductModal from "../components/add.productModal";
 
 const Products = () => {
-  const handleAdd = () => {};
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow(!show);
+  };
   return (
     <div className="w-100 px-5 py-3">
       <div className="d-flex justify-content-between py-3">
         <h3>Products</h3>
-        <Button onClick={handleAdd}>Add new product</Button>
+        <Button onClick={handleShow}>Add new product</Button>
       </div>
       <div>
-        <ProductModal />
+        <ProductModal show={show} setShow={setShow} />
       </div>
     </div>
   );
